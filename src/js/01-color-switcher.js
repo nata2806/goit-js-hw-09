05404d3fc0141be59357249283347ab35b4e6890
nata2806 <vysochina_natalja@ukr.net>
2023-07-23 21:33:16 +0300

    11

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body')
+
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+let timeId = null;
+
+function onClickStart(evt) {
+    timeId = setInterval(changeColor, 1000);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+}
+
+function changeColor() {
+    body.style.backgroundColor = getRandomHexColor()
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onClickStop(evt) {
+    clearInterval(timeId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+}
